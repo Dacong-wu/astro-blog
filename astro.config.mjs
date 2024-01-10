53cc6318d7605f486799162f8186597ccef1a5a8
Dacong-wu <2420390517@qq.com>
2024-01-10 11:23:33 +0800

    bug-24-002
    add astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import partytown from '@astrojs/partytown';
+import compress from 'astro-compress';
 import icon from 'astro-icon';
 import tasks from './src/utils/tasks';
 
@@ -53,6 +54,14 @@ export default defineConfig({
     ),
 
     tasks(),
+    compress({
+      CSS: true,
+      HTML: false,
+      Image: false,
+      JavaScript: true,
+      SVG: true,
+      Logger: 1,
+    }),
   ],
 
   markdown: {
